0b9f87762277cc6d5110485fc9b8ec661658e89f
classy-madafaqa <ahmedd.abd.allah@gmail.com>
2016-03-14 00:35:54 +0200

    Setting up project dependencies

---
+++ src/data/control/excelSheet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data.control;
+
+// imports
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class excelSheet {
+    
+}
--- src/health/monitor/HealthMonitor.java
+++ src/health/monitor/HealthMonitor.java
@@ -5,6 +5,8 @@
  */
 package health.monitor;
 
+import javax.swing.JFrame;
+import GUI.mainJframe;
 /**
  *
  * @author Ahmed
@@ -16,6 +18,8 @@ public class HealthMonitor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame theFrame = new mainJframe();
+        theFrame.setVisible(true);
     }
     
 }
